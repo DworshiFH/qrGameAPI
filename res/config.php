8c66ab136fe76e2c15cac1a2f721ac939567b673
csdc23vz_dworscaka <andreas.dworscak@stud.fh-campuswien.ac.at>
2022-12-16 14:07:00 +0100

    Secrets redacted

--- res/config.php
+++ res/config.php
@@ -2,10 +2,10 @@
 //Database credentials.
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'vr');
-define('DB_PASSWORD', 'REDACTED'); //Password redacted, for security purposes
+define('DB_PASSWORD', 'REDACTED'); //Secret redacted, for security purposes
 define('DB_NAME', 'qr_game');
 
-/* Attempt to connect to MySQL database */
+// Attempt to connect to MySQL database
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
@@ -13,4 +13,3 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     echo("ERROR: Could not connect. " . mysqli_connect_error());
 }
-//echo "connected successfully";
