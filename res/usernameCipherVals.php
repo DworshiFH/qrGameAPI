0c1e8e63f1d75d9b0fcaa647a94268502a4f9a63
csdc23vz_dworscaka <andreas.dworscak@stud.fh-campuswien.ac.at>
2022-12-16 14:05:28 +0100

    Secrets redacted

--- res/usernameCipherVals.php
+++ res/usernameCipherVals.php
@@ -2,6 +2,6 @@
 $ciphering = "AES-256-CBC";
 $iv_length = openssl_cipher_iv_length($ciphering);
 $options   = 0;
-$encryption_iv = 'RX3t1e9erSX43lpk';
-$encryption_key = "B2MAYc6zu6YN5WMvie16";
+$encryption_iv = 'REDACTED'; //Secret redacted, for security purposes
+$encryption_key = "REDACTED"; //Secret redacted, for security purposes
 ?>
